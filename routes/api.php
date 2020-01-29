e522e6c0d99b861ecf4a007c5e3adf7f554359cd
wcluigi <49840319+wcluigi@users.noreply.github.com>
2020-01-29 16:49:24 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 
 Route::any('znajzel-exercise-1', 'Exercise1');
 Route::any('fe4-znajzel-ex2', 'Exercise2');
-Route::any('znajzel-exercise-3', 'Exercise3@index');
-Route::any('znajzel-exercise-3/store', 'Exercise3@store');
+Route::any('fe4-znajzel-ex3', 'Exercise3@index');
+Route::any('fe4-znajzel-3/store', 'Exercise3@store');
 Route::any('znajzel-exercise-final', 'ExerciseFinal@login');
 Route::any('znajzel-exercise-final/store', 'ExerciseFinal@store');
